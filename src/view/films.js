63c882e7a5020bfede52a278f248b4f2a19a4722
Dmitry Kilin <coding@kilin.biz>
2021-11-30 21:04:03 +0300

    feat:  split to elements

---
+++ src/view/films.js
@@ -0,0 +1,13 @@
+export const createFilmsTemplate = () => `
+  <section class="films">
+    <section class="films-list">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+    </section>
+    <section class="films-list films-list--extra">
+      <h2 class="films-list__title">Top rated</h2>
+    </section>
+    <section class="films-list films-list--extra">
+      <h2 class="films-list__title">Most commented</h2>
+    </section>
+  </section>
+`;
