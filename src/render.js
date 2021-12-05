444667e6da18738586bcba308bbe4c96e7b00093
Dmitry Kilin <coding@kilin.biz>
2021-12-05 19:59:03 +0300

    feat: add creatElement n renderElement functions

--- src/render.js
+++ src/render.js
@@ -1,3 +1,27 @@
 export const renderTemplate = (container, template, place = 'beforeend') => {
   container.insertAdjacentHTML(place, template);
 };
+
+export const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case 'beforebegin':
+      container.before(element);
+      break;
+    case 'afterbegin':
+      container.prepend(element);
+      break;
+    case 'beforeend':
+      container.append(element);
+      break;
+    case 'afterend':
+      container.after(element);
+      break;
+  }
+};
