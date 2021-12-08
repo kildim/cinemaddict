ccf3203fad2f333891690bfc59a10487a80b59e7
Dmitry Kilin <coding@kilin.biz>
2021-12-08 12:48:22 +0300

    feat:  add No films screen

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,7 @@
 export const EMOTIONS = ['smile', 'sleeping', 'puke', 'angry'];
+export const filters = {
+  allMovies: 'All movies',
+  watchlist: 'Watchlist',
+  history: 'History',
+  favorites: 'Favorites'
+};
