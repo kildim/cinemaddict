66ebf7690d00dc34ac591341010573cd518a857a
Dmitry Kilin <coding@kilin.biz>
2021-12-08 12:48:22 +0300

    feat:  add No films screen

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import {render} from './render';
 import UserProfile from './view/user-profile';
 import MainMenu from './view/main-menu';
 import Sort from './view/sort';
-import Films from './view/films';
+import ListsContainer from './view/lists-container';
 import List from './view/list';
 import ShowMore from './view/show-more';
 import FooterStatistics from './view/footer-statistics';
 import Card from './view/card';
 import {getFilms, getWatchInfo} from './data/data-adapter';
 import FilmDetails from './view/film-details';
+import FilmsEmpty from './view/films-empty';
+import {filters} from './constants';
+import AllMoviesContainer from './view/all-movies-container';
+import ExtrasContainer from './view/extras-container';
 
 export const LIST_FILMS_CHUNK = 5;
 export const LIST_EXTRAS_CHUNK = 2;
@@ -30,6 +34,8 @@ const mainElement = document.querySelector('.main');
 const footerElement = document.querySelector('.footer');
 
 const filmDetails = new FilmDetails();
+const filmsEmpty = new FilmsEmpty();
+
 const showFilmDetails = (film) => () => {
   filmDetails.init(film);
   render(document.body, filmDetails.element);
@@ -37,53 +43,68 @@ const showFilmDetails = (film) => () => {
 
 render(headerElement, new UserProfile().element);
 render(mainElement, new MainMenu(watchInfo).element);
-render(mainElement, new Sort().element);
-render(mainElement, new Films().element);
 
-const [listFilms, listTopRated, listMostCommented] = document.querySelectorAll('.films-list');
+const listsContainerElement = new ListsContainer().element;
 
-let listFilmsTail = Math.min(films.length, LIST_FILMS_CHUNK);
-let listFilmsHead = 0;
-let listFilmsSampling = films.slice(listFilmsHead, listFilmsTail);
+if (films.length > 0) {
+  render(mainElement, new Sort().element);
+  render(mainElement, listsContainerElement);
 
-const listTopRatedSampling = films.slice(0, LIST_EXTRAS_CHUNK);
-const listMostCommentedSampling = films.slice(0, LIST_EXTRAS_CHUNK);
-
-const filmsList = new List();
-render(listFilms, filmsList.element);
-
-let showMore = null;
-if (films.length > LIST_FILMS_CHUNK) {
-  showMore = new ShowMore();
-  render(listFilms, showMore.element);
-}
-
-render(listTopRated, new List().element);
-render(listMostCommented, new List().element);
-
-const listFilmsContainer = listFilms.querySelector('.films-list__container');
-const listTopRatedContainer = listTopRated.querySelector('.films-list__container');
-const listMostCommentedContainer = listMostCommented.querySelector('.films-list__container');
-
-renderCards(listFilmsContainer, listFilmsSampling, {clickCardHandler: showFilmDetails});
-renderCards(listTopRatedContainer, listTopRatedSampling, {clickCardHandler: showFilmDetails});
-renderCards(listMostCommentedContainer, listMostCommentedSampling, {clickCardHandler: showFilmDetails});
+  const listFilms = new AllMoviesContainer();
+  const listTopRated = new ExtrasContainer('Top rated');
+  const listMostCommented = new ExtrasContainer('Most commented');
+  render(listsContainerElement, listFilms.element);
+  render(listsContainerElement, listTopRated.element);
+  render(listsContainerElement, listMostCommented.element);
 
 
-const onClickShowMoreHandler = (event)=> {
-  event.preventDefault();
-  listFilmsHead = listFilmsTail;
-  listFilmsTail += LIST_FILMS_CHUNK;
-  if (listFilmsTail > films.length) {
-    listFilmsTail = films.length;
-    showMore.removeElement();
-  }
-  listFilmsSampling = films.slice(listFilmsHead, listFilmsTail);
+  let listFilmsTail = Math.min(films.length, LIST_FILMS_CHUNK);
+  let listFilmsHead = 0;
+  let listFilmsSampling = films.slice(listFilmsHead, listFilmsTail);
+
+  const listTopRatedSampling = films.slice(0, LIST_EXTRAS_CHUNK);
+  const listMostCommentedSampling = films.slice(0, LIST_EXTRAS_CHUNK);
+
+  const listFilmsContainer = listFilms.getContainer();
+  const listTopRatedContainer = listTopRated.getContainer();
+  const listMostCommentedContainer = listMostCommented.getContainer();
+
   renderCards(listFilmsContainer, listFilmsSampling, {clickCardHandler: showFilmDetails});
-};
+  renderCards(listTopRatedContainer, listTopRatedSampling, {clickCardHandler: showFilmDetails});
+  renderCards(listMostCommentedContainer, listMostCommentedSampling, {clickCardHandler: showFilmDetails});
 
-if (showMore) {
-  showMore.element.addEventListener('click', onClickShowMoreHandler);
+  let showMore = null;
+  if (films.length > LIST_FILMS_CHUNK) {
+    showMore = new ShowMore();
+    render(listFilms.element, showMore.element);
+  }
+
+  const onEscapeKeyDownHandler = (event) => {
+    if (event.key === 'Escape' || event.key === 'Esc') {
+      event.preventDefault();
+      filmDetails.removeElement();
+    }
+  };
+  const onClickShowMoreHandler = (event)=> {
+    event.preventDefault();
+    listFilmsHead = listFilmsTail;
+    listFilmsTail += LIST_FILMS_CHUNK;
+    if (listFilmsTail > films.length) {
+      listFilmsTail = films.length;
+      showMore.removeElement();
+    }
+    listFilmsSampling = films.slice(listFilmsHead, listFilmsTail);
+    renderCards(listFilmsContainer, listFilmsSampling, {clickCardHandler: showFilmDetails});
+  };
+
+  if (showMore) {
+    showMore.element.addEventListener('click', onClickShowMoreHandler);
+  }
+  document.addEventListener('keydown', onEscapeKeyDownHandler);
+} else {
+  filmsEmpty.init(filters.allMovies);
+  render(mainElement, listsContainerElement);
+  render(listsContainerElement, filmsEmpty.element);
 }
 
 render(footerElement, new FooterStatistics(films.length).element);
---
+++ src/view/lists-container.js
@@ -0,0 +1,26 @@
+import {createElement} from '../render';
+
+const createListsContainerTemplate = () => `
+  <section class="films">
+  </section>
+`;
+
+export default class ListsContainer {
+  #element = null;
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  get template() {
+    return createListsContainerTemplate();
+  }
+
+  removeElement() {
+    this.#element.remove();
+  }
+}
